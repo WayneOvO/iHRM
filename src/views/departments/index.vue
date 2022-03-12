db175d45731885c9e4540cd7f4d636b51ae86ca2
WayneOvO <zhangzhiming7016@gmail.com>
2022-03-12 22:43:55 +0800

    完成树形结构的显示

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,17 +1,94 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        组织架构
-      </h2>
+      <!--头部-->
+      <el-card class="tree-card">
+        <el-row
+          type="flex"
+          justify="space-between"
+          align="middle"
+          style="height: 40px"
+        >
+          <el-col>
+            <span>江苏传智播客教育科技股份有限公司</span>
+          </el-col>
+          <el-col :span="4">
+            <el-row type="flex" justify="end">
+              <el-col>负责人</el-col>
+              <el-col>
+                <el-dropdown>
+                  <span class="el-dropdown-link">
+                    操作<i class="el-icon-arrow-down el-icon--right" />
+                  </span>
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>增加子部门</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </el-col>
+        </el-row>
+
+        <!--tree-->
+        <el-tree :data="departments" :props="defaultProps" default-expand-all>
+          <template v-slot="{ data }">
+            <el-row
+              type="flex"
+              justify="space-between"
+              align="middle"
+              style="height: 40px; width: 100%"
+            >
+              <el-col>
+                <span>{{ data.name }}</span>
+              </el-col>
+              <el-col :span="4">
+                <el-row type="flex" justify="end">
+                  <el-col>{{ data.manager }}</el-col>
+                  <el-col>
+                    <el-dropdown>
+                      <span class="el-dropdown-link">
+                        操作<i class="el-icon-arrow-down el-icon--right" />
+                      </span>
+                      <el-dropdown-menu slot="dropdown">
+                        <el-dropdown-item>增加部门</el-dropdown-item>
+                        <el-dropdown-item>编辑部门</el-dropdown-item>
+                        <el-dropdown-item>删除部门</el-dropdown-item>
+                      </el-dropdown-menu>
+                    </el-dropdown>
+                  </el-col>
+                </el-row>
+              </el-col>
+            </el-row>
+          </template>
+        </el-tree>
+        <!--/tree-->
+      </el-card>
+      <!--/头部-->
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      departments: [
+        { name: '总裁办', manager: '曹操', children: [{ name: '董事会', manager: '曹丕' }] },
+        { name: '行政部', manager: '刘备' },
+        { name: '人事部', manager: '孙权' }
+      ],
+      defaultProps: {
+        label: 'label',
+        children: 'children'
+      }
+    }
+  }
+}
 </script>
 
-<style>
-
+<style scoped>
+.tree-card {
+  padding: 30px 140px;
+  font-size: 14px;
+}
 </style>

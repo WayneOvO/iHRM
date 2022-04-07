0fafb123ee4252fc20ad3a13f35ada5f4c24c9d7
WayneOvO <zhangzhiming7016@gmail.com>
2022-04-07 21:57:23 +0800

    建立公司页面的基本结构

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,15 +1,76 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        公司设置
-      </h2>
+      <el-card>
+        <el-tabs v-model="activeName">
+          <!--角色管理-->
+          <el-tab-pane label="角色管理" name="first">
+            <el-row style="height: 50px">
+              <el-button type="primary" size="small" icon="el-icon-plus">新增角色</el-button>
+            </el-row>
+
+            <!--表格组件-->
+            <el-table border>
+              <el-table-column label="序号" width="120">1</el-table-column>
+              <el-table-column label="角色名" width="240">1</el-table-column>
+              <el-table-column label="描述">1</el-table-column>
+              <el-table-column label="操作">
+                <el-button size="small" type="success">分配权限</el-button>
+                <el-button size="small" type="primary">编辑</el-button>
+                <el-button size="small" type="danger">删除</el-button>
+              </el-table-column>
+            </el-table>
+
+            <!--分页组件-->
+            <el-row type="flex" justify="center" align="middle" style="height: 60px">
+              <el-pagination
+                background
+                layout="prev, pager, next"
+                :total="1000"
+              />
+            </el-row>
+          </el-tab-pane>
+
+          <!--公司信息-->
+          <el-tab-pane label="公司信息" name="second">
+            <el-alert
+              title="对公司名称、公司地址、营业执照、公司地区的更新，将使得公司资料被重新审核，请谨慎修改"
+              :closable="false"
+              type="info"
+              show-icon
+            />
+            <el-form label-width="120px" style="margin-top: 50px">
+              <el-form-item label="企业名称">
+                <el-input disabled style="width:400px" />
+              </el-form-item>
+              <el-form-item label="公司地址">
+                <el-input disabled style="width:400px" />
+              </el-form-item>
+              <el-form-item label="电话">
+                <el-input disabled style="width:400px" />
+              </el-form-item>
+              <el-form-item label="邮箱">
+                <el-input disabled style="width:400px" />
+              </el-form-item>
+              <el-form-item label="备注">
+                <el-input type="textarea" :rows="3" disabled style="width:400px" />
+              </el-form-item>
+            </el-form>
+          </el-tab-pane>
+        </el-tabs>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      activeName: 'first'
+    }
+  }
+}
 </script>
 
 <style>
